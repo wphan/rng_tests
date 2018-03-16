cfadb912f351e7d7874f0285a87841a4b7a80b7b
William Phan <wphan@live.ca>
2018-03-15 23:01:29 -0700

    Initial commit with monobit test

---
+++ README.md
@@ -0,0 +1,24 @@
+## Install dependencies
+```
+# create virtualenvironment
+virtualenv venv
+
+# activate venv (POSIX)
+source venv/bin/activate
+
+# activate venv (Windows)
+source venv/scripts/activate
+
+# install deps
+pip install -r requirements.txt
+```
+
+## Usage
+```
+# run single test
+cd rng_tests
+python monobit.py
+
+# run everything
+#TODO
+```
---
+++ rng_tests/frequency_monobit.py
@@ -0,0 +1,33 @@
+import random
+import math
+
+from scipy.special import erfc
+
+
+def monobit(bit_string):
+    """
+    http://qrng.b-phot.org/static/media/NistTestsLongDescription.pdf
+
+    :param bit_string: string of bits 
+    :return p-value: result of monobit test
+    """
+    count = 0
+    for bit in bit_string:
+        count += (2*int(bit) - 1)
+
+    observed_sum = abs(count) / math.sqrt(len(bit_string))
+    p_value = erfc(observed_sum / math.sqrt(2))
+
+    return p_value
+
+if __name__ == "__main__":
+    # test not very random numbers
+    test_ones = format(2**256 - 1, "0b")
+    print("input: 2^256 - 1, output: {}".format(monobit(test_ones)))
+
+    test_zeroes = ''.join(["0" for i in list(range(256))])
+    print("input: 0, output: {}".format(monobit(test_zeroes)))
+
+    # test with random number
+    test_random = format(random.getrandbits(256), '0b')
+    print("input: random 256-bit number, output: {}".format(monobit(test_random)))
